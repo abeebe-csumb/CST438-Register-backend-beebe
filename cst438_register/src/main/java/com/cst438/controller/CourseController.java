ea4ef72149edd2b0754764891419a3dea7603bdd
abeebe-csumb <abeebe@csumb.edu>
2021-09-28 19:54:20 -0700

    Add call to gradebook REST & controller class for courses to update final grades for a student. Add testing for RabbitMQ.

--- cst438_register/src/main/java/com/cst438/controller/CourseController.java
+++ cst438_register/src/main/java/com/cst438/controller/CourseController.java
@@ -1,5 +1,7 @@
 package com.cst438.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +9,25 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cst438.domain.Course;
 import com.cst438.domain.CourseDTOG;
+import com.cst438.domain.CourseRepository;
 import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.Student;
+import com.cst438.domain.StudentRepository;
 
 @RestController
 public class CourseController {
 	
 	@Autowired
 	EnrollmentRepository enrollmentRepository;
+	
+	@Autowired
+	CourseRepository courseRepository;
+	
+	@Autowired
+	StudentRepository studentRepository;
 	
 	/*
 	 * endpoint used by gradebook service to transfer final course grades
@@ -24,8 +36,18 @@ public class CourseController {
 	@Transactional
 	public void updateCourseGrades( @RequestBody CourseDTOG courseDTO, @PathVariable("course_id") int course_id) {
 		
-		//TODO  complete this method in homework 4
+		List<CourseDTOG.GradeDTO> grades = courseDTO.grades;
+		Course course  = courseRepository.findByCourse_id(courseDTO.course_id);
 		
+		for (CourseDTOG.GradeDTO g : grades) {
+			Student student = studentRepository.findByEmail(g.student_email);
+			Enrollment e = new Enrollment();
+			e.setStudent(student);
+			e.setCourse(course);
+			e.setCourseGrade(g.grade);
+			
+			enrollmentRepository.save(e);
+		}
 	}
 
 }
--- cst438_register/src/main/java/com/cst438/service/GradebookServiceREST.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceREST.java
@@ -1,12 +1,25 @@
 package com.cst438.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
+import com.cst438.domain.Course;
+import com.cst438.domain.CourseRepository;
+import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentDTO;
+import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.Student;
+import com.cst438.domain.StudentRepository;
 
 
 public class GradebookServiceREST extends GradebookService {
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+	@Autowired
+	CourseRepository courseRepository;
 
 	private RestTemplate restTemplate = new RestTemplate();
 
@@ -19,9 +32,14 @@ public class GradebookServiceREST extends GradebookService {
 
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
+		Student student = studentRepository.findByEmail(student_email);
+		Course course  = courseRepository.findByCourse_id(course_id);
 		
-		//TODO  complete this method in homework 4
+		Enrollment enrollment = new Enrollment();
+		enrollment.setStudent(student);
+		enrollment.setCourse(course);
 		
+		restTemplate.postForEntity("localhost:8081/enrollment", enrollment, null);
 	}
 
 }
